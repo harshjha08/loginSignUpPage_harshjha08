b47dc432689be8e246d2bc171c2a49d45ab201b3
Harsh Jha <hjha1289@gmail.com>
2025-09-05 15:12:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showLogin(){
+    document.querySelector(".innerContainer2").classList.add("active");
+        document.querySelector(".innerContainer1").classList.remove("active");
+
+}
+function showRegister(){
+     document.querySelector(".innerContainer1").classList.add("active");
+        document.querySelector(".innerContainer2").classList.remove("active");
+}
